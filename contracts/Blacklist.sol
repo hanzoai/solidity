d1795a03f89ca7c9bd22f3e818c687bea0e99de6
Zach Kelling <z@zeekay.io>
2018-07-05 20:32:26 -0400

    Check for uninitialized mappings

--- contracts/Blacklist.sol
+++ contracts/Blacklist.sol
@@ -5,6 +5,7 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Blacklist is Ownable {
     mapping (address => bool) blacklist;
+    uint blacklistNum;
 
     event AddedToBlacklist(address indexed addr);
     event RemovedFromBlacklist(address indexed addr);
@@ -13,15 +14,20 @@ contract Blacklist is Ownable {
 
     function addToBlacklist(address addr) public onlyOwner {
         blacklist[addr] = true;
+        blacklistNum++;
         emit AddedToBlacklist(addr);
     }
 
     function removeFromBlacklist(address addr) public onlyOwner {
         blacklist[addr] = false;
+        blacklistNum--;
         emit RemovedFromBlacklist(addr);
     }
 
     function isBlacklisted(address addr) public view returns (bool) {
+        if (blacklistNum == 0) {
+            return false;
+        }
         return blacklist[addr];
     }
 }
--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -5,6 +5,7 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Whitelist is Ownable {
     mapping (address => bool) whitelist;
+    uint whitelistNum;
 
     event AddedToWhitelist(address indexed addr);
     event RemovedFromWhitelist(address indexed addr);
@@ -13,15 +14,20 @@ contract Whitelist is Ownable {
 
     function addToWhitelist(address addr) public onlyOwner {
         whitelist[addr] = true;
+        whitelistNum++;
         emit AddedToWhitelist(addr);
     }
 
     function removeFromWhitelist(address addr) public onlyOwner {
         whitelist[addr] = false;
+        whitelistNum++;
         emit RemovedFromWhitelist(addr);
     }
 
     function isWhitelisted(address addr) public view returns (bool) {
+        if (whitelistNum == 0) {
+            return false;
+        }
         return whitelist[addr];
     }
 }
