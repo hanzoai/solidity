73f734bb25e552777f25b0d3df22947cb8d797e0
Ari <arilerner@mac.com>
2018-07-08 17:11:23 -0400

    Added `require(addr != address(0));` check in Blacklist

--- contracts/Blacklist.sol
+++ contracts/Blacklist.sol
@@ -13,18 +13,21 @@ contract Blacklist is Ownable {
     constructor() Ownable() public {}
 
     function addToBlacklist(address addr) public onlyOwner {
+        require(addr != address(0));
         blacklist[addr] = true;
         blacklistNum++;
         emit AddedToBlacklist(addr);
     }
 
     function removeFromBlacklist(address addr) public onlyOwner {
+        require(addr != address(0));
         blacklist[addr] = false;
         blacklistNum--;
         emit RemovedFromBlacklist(addr);
     }
 
     function isBlacklisted(address addr) public view returns (bool) {
+        require(addr != address(0));
         if (blacklistNum == 0) {
             return false;
         }
